73447737f7f9550b1d321046e80d1e59475ee375
Huy Pham <huypham1700@gmail.com>
2020-11-23 11:04:46 +0700

    new project

---
+++ Nodejs/models/Category.js
@@ -0,0 +1,33 @@
+const {
+  DataTypes
+} = require('sequelize');
+
+module.exports = sequelize => {
+  const attributes = {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: null,
+      primaryKey: true,
+      autoIncrement: true,
+      comment: null,
+      field: "id"
+    },
+    name: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: null,
+      field: "name"
+    }
+  };
+  const options = {
+    tableName: "Category",
+    comment: "",
+    indexes: []
+  };
+  const CategoryModel = sequelize.define("Category_model", attributes, options);
+  return CategoryModel;
+};
