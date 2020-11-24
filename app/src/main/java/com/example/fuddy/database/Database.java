b6ec2fe8c447b12429111c7f673ef00321d6ac00
Huy Pham <huypham1700@gmail.com>
2020-11-24 12:30:23 +0700

    l

--- app/src/main/java/com/example/fuddy/database/Database.java
+++ app/src/main/java/com/example/fuddy/database/Database.java
@@ -5,11 +5,14 @@ import android.content.Context;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import com.example.fuddy.database.dao.FoodDAO;
+import com.example.fuddy.database.dao.UserDAO;
+import com.example.fuddy.entity.Food;
 import com.example.fuddy.entity.User;
 
-@androidx.room.Database(entities = {User.class},version = 1)
+@androidx.room.Database(entities = {User.class, Food.class},version = 1)
 public abstract class Database extends RoomDatabase {
-    private static String DB_NAME = "Fuddy";
+    private static final String DB_NAME = "Fuddy";
     private static Database instance;
 
     public static synchronized Database getInstance(Context context){
@@ -19,4 +22,7 @@ public abstract class Database extends RoomDatabase {
         }
         return instance;
     }
+    public abstract UserDAO userDAO();
+    public abstract FoodDAO foodDAO();
+
 }
