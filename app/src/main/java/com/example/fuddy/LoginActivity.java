b1a8310b8f2b934c5bc2742153879d1ad17aee66
TungNT. Tung Nguyen Thanh <TungNT@vietis.vn>
2020-11-24 17:12:25 +0700

    aaaaaaaaa
    aaaaaaaa

--- app/src/main/java/com/example/fuddy/LoginActivity.java
+++ app/src/main/java/com/example/fuddy/LoginActivity.java
@@ -2,13 +2,32 @@ package com.example.fuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView textView5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        textView5 = findViewById(R.id.textView5);
+        textView5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
+                        finish();
+                    }
+                }, 6000);
+            }
+        });
     }
 }
