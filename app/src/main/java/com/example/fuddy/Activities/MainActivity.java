589022469e92fb33700bb063405a831f4ba52d29
TungNT. Tung Nguyen Thanh <TungNT@vietis.vn>
2020-11-26 17:04:23 +0700

    update
    update

--- app/src/main/java/com/example/fuddy/Activities/MainActivity.java
+++ app/src/main/java/com/example/fuddy/Activities/MainActivity.java
@@ -1,22 +1,15 @@
 package com.example.fuddy.Activities;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-
-import com.example.fuddy.HomeFragment;
-import com.example.fuddy.NotificationsFragment;
-import com.example.fuddy.OrderFragment;
 import com.example.fuddy.R;
-import com.example.fuddy.SettingFragment;
-import com.example.fuddy.StoreFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,42 +21,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         toolbar = getSupportActionBar();
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
         toolbar.setTitle("Home");
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             Fragment fragment;
-            switch (item.getItemId()){
+            switch (item.getItemId()) {
                 case R.id.navigation_home:
                     toolbar.setTitle("Home");
-                    fragment = new HomeFragment();
-                    loadFragment(fragment);
                     return true;
                 case R.id.navigation_store:
                     toolbar.setTitle("Store");
-                    fragment = new StoreFragment();
-                    loadFragment(fragment);
                     return true;
                 case R.id.navigation_order:
                     toolbar.setTitle("Order");
-                    fragment = new OrderFragment();
-                    loadFragment(fragment);
                     return true;
                 case R.id.navigation_setting:
                     toolbar.setTitle("Setting");
-                    fragment = new SettingFragment();
-                    loadFragment(fragment);
                     return true;
                 case R.id.navigation_notifications:
                     toolbar.setTitle("Notifications");
-                    fragment = new NotificationsFragment();
-                    loadFragment(fragment);
-                    return true;
             }
             return false;
         }
